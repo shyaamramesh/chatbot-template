365f815f82f7b909ee7c0c259f76b268b17b60ab
Shyaam Ramesh <49405098+shyaamramesh@users.noreply.github.com>
2019-04-09 09:07:27 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 import random
 import webbrowser
 from random import randint
+#Be sure to import all modules that are requires for the run_codes
 
 greetings = ['hello', 'hi']
 random_greeting = random.choice(greetings)
@@ -9,9 +10,9 @@ question = ['whats your name','how are you']
 responses = ['i dont have one',"I'm fine"]
 
 task = ['open firefox', 'give me a random number']
-run_code = ["webbrowser.open_new('https://www.google.com/')", "print(randint(0,100000))"]
+run_code = ["webbrowser.open_new('https://www.google.com/')", "print(randint(0,100000))"] #python string, to be executed
 
-while True:
+while True:N
 	userInput = raw_input(">>> ")
 	if userInput in greetings:
 		print(random_greeting)
@@ -19,5 +20,5 @@ while True:
 		print(responses[question.index(userInput)])
 	elif userInput in task:
 		exec(run_code[task.index(userInput)])
-	else:
+	else: #if the user says something random
 		print("I did not understand what you said")
